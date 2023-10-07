ee2ad77c6ae8fbf84298936554572b50aadefd32
Vitaliy Irtlach <w13vitaliy@gmail.com>
2023-10-07 18:14:58 +0300

    Initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,90 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="max-w-[900px] mx-auto py-12">
+      <Head>
+        <title>Vitaliy Irtlach - Fullstack Software Engineer</title>
+      </Head>
+      <div className="flex items-center gap-3">
+        <img src="/photo.jpeg" className="w-12 h-12 rounded-full" />
+        <div>
+          <p className="text-2xl font-bold">Vitaliy Irtlach</p>
+          <p className="text-gray-500 text-sm">Fullstack Software Engineer</p>
         </div>
       </div>
+      <div className="mt-4 text-gray-700 space-y-4">
+        <p>
+          My name is Vitaly Irtlach, and I was born on September 13, 2005 in the
+          city of Dnipro, Ukraine. I am a software developer with experience in
+          startups and commercial platforms.
+        </p>
+        <p>
+          In 2023, I started studying at the{" "}
+          <a href="https://www.dnu.dp.ua/en" target="_blank">
+            Oles Honchar Dnipro National University
+          </a>
+          , majoring in computer engineering.
+        </p>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <p>
+          I am currently working as a Fullstack JavaScript developer. My
+          responsibilities include software development, outsourcing,
+          interviewing job candidates, interviewing software customers, managing
+          technical aspects of projects, mentoring and many other tasks.
+        </p>
+        <p>
+          As part of this experience, I participated in the development of
+          various projects, such as a service for investors, a site for finding
+          and managing metalworking enterprises, a service for algotrading and
+          many others. It was an intensive experience that helped me develop my
+          skills and solve challenging tasks.
+        </p>
+
+        <p>
+          In my free time, I enjoy sports, especially fitness, and learning new
+          technologies. I like to keep my finger on the pulse of innovation and
+          always try to stay on top of the latest technology trends.
+        </p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <hr className="my-6" />
+      <div className="flex items-center gap-4">
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://github.com/vitaliyirtlach"
           target="_blank"
-          rel="noopener noreferrer"
+          className="hover:text-black"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+          GitHub
         </a>
-
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://www.instagram.com/vitaliyirtlach/"
           target="_blank"
-          rel="noopener noreferrer"
+          className="hover:text-pink-600"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
+          Instagram
         </a>
-
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://t.me/vitaliyirtlach"
           target="_blank"
-          rel="noopener noreferrer"
+          className="hover:text-blue-400"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
+          Telegram
         </a>
-
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://www.upwork.com/freelancers/~01c2b7dfc502099e16"
           target="_blank"
-          rel="noopener noreferrer"
+          className="hover:text-green-600"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          Upwork
+        </a>
+        <a
+          href="https://twitter.com/vitaliyirtlach"
+          target="_blank"
+          className="hover:text-black"
+        >
+          X
         </a>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
